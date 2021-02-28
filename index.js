40731576a3805215bb38a907485dc31d39bdaeb6
nadellasaideep1 <64152721+nadellasaideep1@users.noreply.github.com>
2021-02-28 11:27:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/UXsdxX74ros?controls=1&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/SAz_HidzhS4?controls=1&loop=1&autoplay=1");
   ifrm.style.border = 'none';
   document.querySelector('#video').appendChild(ifrm);
 }
